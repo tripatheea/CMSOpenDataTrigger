8e48af33fa41413f1361c13bd90a8d647a65982c
Aashish <helloworld@aashishtripathee.com>
2015-03-28 13:43:15 -0400

    triggers

---
+++ python/cmsopendatatrigger_cfi.py
@@ -0,0 +1,3 @@
+import FWCore.ParameterSet.Config as cms
+
+CMSOpenDataTrigger = cms.EDProducer('CMSOpenDataTrigger')
---
+++ src/CMSOpenDataTrigger.cc
@@ -0,0 +1,195 @@
+#include <memory>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DataFormats/Common/interface/HLTGlobalStatus.h"
+#include "DataFormats/Provenance/interface/ParameterSetID.h"
+
+
+#include "DataFormats/Common/interface/TriggerResults.h"
+
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+#include "FWCore/Common/interface/TriggerNames.h"
+
+#include "FWCore/Common/interface/TriggerResultsByName.h"
+
+
+#include "HLTrigger/HLTcore/interface/HLTConfigProvider.h"
+
+using namespace std;
+using namespace edm;
+using namespace trigger;
+
+
+class CMSOpenDataTrigger : public edm::EDProducer {
+   public:
+      explicit CMSOpenDataTrigger(const edm::ParameterSet&);
+      ~CMSOpenDataTrigger();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+   private:
+      virtual void beginJob() ;
+      virtual void produce(edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+      
+      virtual void beginRun(edm::Run&, edm::EventSetup const&);
+      virtual void endRun(edm::Run&, edm::EventSetup const&);
+      virtual void beginLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&);
+      virtual void endLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&);
+      
+      bool triggerFired(const std::string& triggerWildCard, const edm::TriggerResults& triggerResults);
+      unsigned int findTrigger(const std::string& triggerWildCard);
+
+      ofstream csvOut_;
+      HLTConfigProvider hltConfig_;      
+      InputTag hltInputTag_;
+};
+
+
+
+
+CMSOpenDataTrigger::CMSOpenDataTrigger(const edm::ParameterSet& iConfig) : 
+	hltConfig_(),
+	hltInputTag_("TriggerResults","","HLT")
+{
+  csvOut_.open("triggers.csv");
+}
+
+
+CMSOpenDataTrigger::~CMSOpenDataTrigger() { }
+
+void CMSOpenDataTrigger::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+}
+
+
+void CMSOpenDataTrigger::beginJob() {
+
+}
+
+void CMSOpenDataTrigger::endJob() {
+
+}
+
+
+void CMSOpenDataTrigger::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
+
+  int eventNumber = iEvent.id().event();
+  int runNumber = iEvent.id().run();
+
+  edm::Handle<edm::TriggerResults> trigResults; 
+
+  iEvent.getByLabel(hltInputTag_, trigResults);
+
+  const vector<string> triggerNames = hltConfig_.triggerNames();
+
+  string triggers[7] = {"HLT_L1Jet6U", "HLT_L1Jet10U", "HLT_Jet15U", "HLT_Jet30U", "HLT_Jet50U", "HLT_Jet70U", "HLT_Jet100U"}; // Only these trigger info will be stored.
+  vector<string> triggersThatMatter(triggers, triggers + sizeof triggers / sizeof triggers[0]);
+
+  for (unsigned int i = 0; i < triggersThatMatter.size(); i++) {
+    string name = triggersThatMatter[i];
+
+    pair<int, int> prescale = hltConfig_.prescaleValues(iEvent, iSetup, name);
+    bool fired = triggerFired(name, ( * trigResults));
+    cout << eventNumber << "," << runNumber << "," << name << "," << fired << "," << prescale.first << "," << prescale.second << endl;
+    csvOut_ << eventNumber << "," << runNumber << "," << name << "," << fired << "," << prescale.first << "," << prescale.second << endl;
+  }
+
+
+  /*
+  // Alternate way to get triggers. 
+  // This is limited in scope; for example, it's extremely difficult to find prescale values this way. 
+  // So use this only for testing stuff.
+  
+  const edm::TriggerNames& trigNames = iEvent.triggerNames(*trigResults);  
+  const vector<string> names = trigNames.triggerNames();
+
+  for(unsigned int i = 0; i < names.size(); i++) {
+    bool accepted = (* trigResults).accept(trigNames.triggerIndex(names[i]));
+    cout << names[i] << ": " << accepted << endl;
+  }
+  */
+
+
+}
+
+
+void CMSOpenDataTrigger::beginRun(edm::Run & iRun, edm::EventSetup const & iSetup){
+
+  bool changed = true;
+  if ( hltConfig_.init(iRun, iSetup, hltInputTag_.process(), changed) ) {
+    // if init returns TRUE, initialisation has succeeded!
+    edm::LogInfo("TopPairElectronPlusJetsSelectionFilter") << "HLT config with process name "
+				<< hltInputTag_.process() << " successfully extracted";
+  }
+  else {
+    edm::LogError("TopPairElectronPlusJetsSelectionFilter_Error")
+				<< "Error! HLT config extraction with process name " << hltInputTag_.process() << " failed";
+  }
+
+}
+
+void CMSOpenDataTrigger::endRun(edm::Run&, edm::EventSetup const&) {
+
+}
+
+void CMSOpenDataTrigger::beginLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&) {
+
+}
+
+void CMSOpenDataTrigger::endLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&) {
+
+}
+
+bool CMSOpenDataTrigger::triggerFired(const std::string& triggerWildCard, const edm::TriggerResults& triggerResults) {
+  bool fired = false;
+  unsigned int index = findTrigger(triggerWildCard);
+
+  if (index < triggerResults.size()) {
+    if (triggerResults.accept(index)) {
+      fired = true;
+    }
+  }
+
+  return fired;
+
+}
+
+unsigned int CMSOpenDataTrigger::findTrigger(const std::string& triggerWildCard) {
+  const std::vector<std::string>& triggers = hltConfig_.triggerNames();
+  unsigned int found = 9999;
+
+  size_t length = triggerWildCard.size();
+  for (unsigned int index = 0; index < triggers.size(); ++index) {
+    if (length <= triggers[index].size() && triggerWildCard == triggers[index].substr(0, length)) {
+      found = index;
+      break;
+    }
+  }
+
+  return found;
+}
+
+
+
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(CMSOpenDataTrigger);
